137d5e1752465ab4f2a2bd3650de9cc7560e41db
Yorbre25 <ybrenesr@estudiantec.cr>
2021-09-02 08:33:59 -0600

    HammingCode

--- main.cpp
+++ main.cpp
@@ -3,20 +3,7 @@
 #include "HammingCode/Hamming.h"
 
 int main(int argc, char *argv[]) {
-//    QCoreApplication a(argc, argv);
-//    qDebug() << "Hello World";
-//    return QCoreApplication::exec();
-    std::vector<bool> vector {1,1,1,1,1};
-    Hamming hamming;
-    hamming.cambiarTipoParidad();
-    hamming.print(vector);
-    vector = hamming.agregarBitsDeParidad(vector);
-    hamming.print(vector);
-    vector = hamming.establecerValoresDeParidad(vector);
-    hamming.print(vector);
-//    vector = hamming.modificarBitEnPos(vector, 3);
-//    hamming.print(vector);
-    std::vector<int> errorEnPos = hamming.revisarParidad(vector);
-    std::cout << "Error en posicion: ";
-
+    QCoreApplication a(argc, argv);
+    qDebug() << "Hello World";
+    return QCoreApplication::exec();
 }
