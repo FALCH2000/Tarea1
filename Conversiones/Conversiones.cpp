235e511a2745087539ae3ecae3301d7552417cf0
Ricardo Gatgens <gatgens48@gmail.com>
2021-09-03 06:41:00 -0600

    Conversiones y gráfico

---
+++ Conversiones/Conversiones.cpp
@@ -0,0 +1,114 @@
+#include "Conversiones.h"
+#include <iostream>
+#include <math.h>
+#include <list>
+
+
+
+using namespace std;
+
+Conversiones::Conversiones() {
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+bool Conversiones::Verificar_Octal(int numero){
+    bool resultado = true;
+
+    while(numero != 0){
+        if(numero%10 >=8){
+            numero = 0;
+            resultado = false;
+        }else{
+            numero = numero/10;
+        }
+        
+    }
+    return resultado;
+}
+
+//------------------------------------------------------------------------------------------------------
+
+int Conversiones::Octal_Decimal(int numero){
+    int resultado = 0;
+    int exponente = 0;
+    int multiplicador = 0;
+
+    while(numero != 0){
+        multiplicador = pow(8,exponente);
+        resultado += numero%10*multiplicador;
+        exponente++;
+        numero = numero/10;
+    }
+
+    return resultado;
+}
+
+
+
+
+    //---------------------------------------------------------------------------------------------------------
+
+    string Conversiones::Octal_Hexadecimal(int numero){
+    list <string> numeros;
+    numero = Octal_Decimal(numero);
+
+    while(numero > 16){
+        numeros.push_front(to_string(numero%16));
+        numero = numero/16;
+    }
+
+    numeros.push_front(to_string(numero));
+
+    
+
+    return Resultado_hexa(numeros);
+}
+//-----------------------------------------------------------------------------------------------------
+string Conversiones::Octal_Binario(int numero){
+    list <string> numeros;
+    numero = Octal_Decimal(numero);
+
+    while(numero > 2){
+        numeros.push_front(to_string(numero%2));
+        numero = numero/2;
+    }
+
+    numeros.push_front(to_string(numero));
+
+    
+
+    return Resultado_hexa(numeros);
+}
+
+//---------------------------------------------------------------------------------------------------------
+    string Conversiones::Resultado_hexa( const list<string> & Lista ){
+
+        string resultado = "";
+
+            list<string>::const_iterator pos;
+            pos = Lista.begin();
+            while( pos != Lista.end())
+        {
+
+            if(*pos == "10"){
+                resultado += "A";
+            }else if(*pos == "11"){
+                resultado += "B";
+            }else if(*pos == "12"){
+                resultado += "C";
+            }else if(*pos == "13"){
+                resultado += "D";
+            }else if(*pos == "14"){
+                resultado += "E";
+            }else if(*pos == "15"){
+                resultado += "F";
+            }else{
+                resultado += *pos;
+            }
+            
+            pos++;
+        }
+    return resultado;
+
+    }
